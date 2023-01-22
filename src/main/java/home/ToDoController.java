974382759dee7297cc61943c9b074c7a4e2d5c17
thenty1109 <92215871+thenty1109@users.noreply.github.com>
2023-01-22 12:33:35 +0800

    Update To-Do List function
    To-Do List function is added to the app for the user to add their events and organise their schedule.

---
+++ src/main/java/home/ToDoController.java
@@ -0,0 +1,95 @@
+package home;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ResourceBundle;
+
+public class ToDoController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        datePicker.setValue(LocalDate.now());
+    }
+
+    @FXML
+    Button addButton;
+
+    @FXML
+    Button homeButton;
+
+    @FXML
+    TextField descriptionTextField;
+
+    @FXML
+    DatePicker datePicker;
+
+    @FXML
+    ListView<LocalEvent> eventList;
+
+    ObservableList<LocalEvent> list = FXCollections.observableArrayList();
+
+    @FXML
+    private void addEvent(Event e){
+        list.add(new LocalEvent(datePicker.getValue(), descriptionTextField.getText()));
+        eventList.setItems(list);
+        refresh();
+    }
+
+    private void refresh(){
+        datePicker.setValue(LocalDate.now());
+        descriptionTextField.setText(null);
+    }
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToHome (ActionEvent event) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("dashboard.fxml"));
+        root = loader.load();
+
+        DashboardController DashboardController = loader.getController();
+        try {
+            File file = new File("username.txt");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            DashboardController.displayName(bufferedReader.readLine());
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        DashboardController.displayDate(String.valueOf(java.time.LocalDate.now()));
+
+        LocalTime currentTime = LocalTime.now();
+        int hour = currentTime.getHour();
+
+        if (hour >= 6 && hour < 18) {
+            DashboardController.hideNight();
+        } else {
+            DashboardController.hideDay();
+        }
+
+        stage = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        scene.getStylesheets().add("style.css");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
